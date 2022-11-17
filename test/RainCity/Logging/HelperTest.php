f1c660799cc57b79235275edea90dc5990c623bc
Blair Cooper <dev@raincitysolutions.com>
2022-11-16 16:30:52 -0800

    Clean up

--- test/RainCity/Logging/HelperTest.php
+++ test/RainCity/Logging/HelperTest.php
@@ -1,9 +1,8 @@
 <?php declare(strict_types=1);
 namespace RainCity\Logging;
 
-use PHPUnit\Framework\TestCase;
 
-class HelperTest extends TestCase
+class HelperTest
 {
 }
 
--- test/RainCity/TestHelper/StubLogger.php
+++ test/RainCity/TestHelper/StubLogger.php
@@ -1,31 +1,12 @@
 <?php declare(strict_types=1);
 namespace RainCity\TestHelper;
 
-use PHPUnit\Framework\TestResult;
 use Psr\Log\LoggerInterface;
 use RainCity\Logging\LoggerIntf;
 
 class StubLogger implements LoggerIntf {
     public static function getLogger(string $loggerName, ?string $loggerKey = null): LoggerInterface {
-        $obj = new class() extends \PHPUnit\Framework\TestCase {
-            public function provides()
-            {}
-
-            public function count()
-            {}
-
-            public function toString()
-            {}
-
-            public function run(TestResult $result = null)
-            {}
-
-            public function requires()
-            {}
-            public function sortId()
-            {}
-
-        };
+        $obj = new class() extends \PHPUnit\Framework\TestCase {};
         return $obj->getMockBuilder(\Psr\Log\LoggerInterface::class)->getMock();
     }
 }
