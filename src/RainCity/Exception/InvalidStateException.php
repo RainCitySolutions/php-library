52964e9b2fa0b486b8e3b8daa3aa18e7320c7248
Blair Cooper <dev@raincitysolutions.com>
2025-01-08 08:41:58 -0800

    Add InvalidStateException

---
+++ src/RainCity/Exception/InvalidStateException.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types = 1);
+namespace RainCity\Exception;
+
+class InvalidStateException extends \Exception
+{
+    /**
+     * {@inheritDoc}
+     * @see \Exception::__construct()
+     */
+    public function __construct(string $message = '""', int $code = null, \Throwable $previous = null): string
+    {
+        parent::__construct('Invalid State: ' . $message, $code = null, $previous = null);
+    }
+}
